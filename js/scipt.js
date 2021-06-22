b054abfb708248c3465a1cc09ccff526f3b5fed5
Roberto <robertoanfuso98@gmail.com>
2021-06-22 18:22:24 +0200

    push per i metodi

---
+++ js/scipt.js
@@ -0,0 +1,28 @@
+const app= new Vue(
+    {
+        el: "#app",
+        data:{
+            counter:0,
+            foto:[
+                "img/img1.jpg",
+                "img/img2.jpg",
+                "img/img3.jpg",
+                "img/img4.jpg"
+            ]
+        },
+        methods:{
+            nextfoto(){
+                this.counter++;
+                if(this.counter==(this.foto.length)){
+                    this.counter=0;
+                }
+            },
+            prevfoto(){
+                this.counter--;
+                if(this.counter<0){
+                    this.counter=this.foto.length -1;
+                }
+            }
+        }
+    }
+);
